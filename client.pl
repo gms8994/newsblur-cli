1087b4ab1cbd5f0fa351cac880822d654e6ed22a
Glendon Solsberry <glen.solsberry@bellwethercorp.com>
2015-08-27 10:21:52 -0400

    Use panes to keep things organized; set statuses in a special container

--- client.pl
+++ client.pl
@@ -5,57 +5,68 @@ use Curses::UI;
 use Text::Unidecode;
 use Time::HiRes qw(gettimeofday);
 
-my $cui = new Curses::UI(
-	-color_support => 1,
-);
+my $cui;
+
+eval "use Curses::UI::POE;";
+
+if (! $@) {
+	$cui = new Curses::UI::POE(
+		-color_support => 1,
+	);
+} else {
+	$cui = new Curses::UI(
+		-color_support => 1,
+	);
+}
 
 $cui->leave_curses();
 
 my $nb = Newsblur->new();
 $nb->set_cui($cui);
 
-my %subscriptions = $nb->get_subscriptions();
+my %subscriptions;
 my %stories;
 my @story_hashes = ();
 my $last_sync_attempt = gettimeofday();
 
-my $sub_list_width = 30;
+my $subscription_list_width = 30;
 my $story_list_height = 10;
 my $selected_story = undef;
 
-my ($mainwindow, $sub_list, $story_list, $story_container);
+my ($mainwindow, $subscription_list, $story_list, $story_container, $status_line);
 
 set_menu();
 set_window();
 
-$sub_list->focus();
+$nb->set_status_line($status_line);
+$subscription_list->focus();
 $cui->mainloop();
 
 sub attempt_sync {
 	my ($message) = @_;
 	$last_sync_attempt = gettimeofday();
 
-	my $result = $nb->mark_as_read(\@story_hashes);
-	@story_hashes = () if ($result);
-	status("Counting is hard: $message", sub {
-		$nb->refresh_feeds();
+	status("Syncing read stories", sub {
+		my $result = $nb->mark_as_read(\@story_hashes);
+		@story_hashes = () if ($result);
 	});
 }
 
 sub draw_posts_window {
-	my ($widget) = @_;
+	my $feed_id = $subscription_list->get();
 
-	my $feed_id = $widget->get();
-
+	status("Getting stories remote");
 	%stories = $nb->get_stories($feed_id);
 	my $title = $subscriptions{$feed_id};
 	$title =~ s/<\/?bold>//g;
 	$story_list->title("Unread stories for " . $title);
 
+	status("Updating the story container");
 	$story_container->text('');
 	$story_container->title('');
 	$story_container->draw();
 
+	status("Updating the stories");
 	update_stories();
 	$story_list->focus();
 }
@@ -69,6 +80,44 @@ sub update_stories {
 	$story_list->labels(\%labels);
 	$story_list->values( [ keys(%stories) ] );
 	$story_list->draw();
+}
+
+sub move_to_next_story {
+	$story_list->focus();
+	$story_list->option_next();
+	$story_list->draw();
+}
+
+sub move_to_previous_story {
+	$story_list->focus();
+	$story_list->option_prev();
+	$story_list->draw();
+}
+
+sub move_to_next_subscription {
+	status("Moving to next subscription");
+	$subscription_list->focus();
+	$subscription_list->option_next();
+	$subscription_list->set_selection($subscription_list->{-ypos});
+	draw_posts_window();
+}
+
+sub move_to_previous_subscription {
+	$subscription_list->focus();
+	$subscription_list->option_prev();
+	$subscription_list->draw();
+	$subscription_list->set_selection($subscription_list->{-ypos});
+	draw_posts_window();
+}
+
+sub mark_all_as_read {
+	foreach my $story_id (@{$story_list->values()}) {
+		$stories{$story_id}{title} =~ s/<\/?bold>//g;
+		push(@story_hashes, $story_id);
+	}
+	update_stories();
+	move_to_next_subscription();
+	attempt_sync();
 }
 
 sub display_content {
@@ -93,7 +142,7 @@ sub display_content {
 			my $wc = new HTML::WikiConverter(
 				dialect => 'Markdown',
 				header_style => 'setext',
-				link_style => 'inline'
+				image_tag_fallback => 0
 			);
 			$content = $wc->html2wiki( $content );
 		}
@@ -108,6 +157,7 @@ sub display_content {
 	$story_container->text($content);
 	$story_container->cursor_to_home();
 	$story_container->draw();
+	$cui->schedule_event(\&attempt_sync);
 
 	if ($_does_not_have_converter) {
 		$cui->status("This would be more readable if you installed the HTML::WikiConverter::Markdown perl module");
@@ -115,7 +165,7 @@ sub display_content {
 		$cui->nostatus;
 	}
 
-	$story_container->focus() if (length($content) > $max_chars);
+	$story_container->focus();
 }
 
 sub set_menu {
@@ -126,6 +176,11 @@ sub set_menu {
 	$cui->set_binding(sub {$menu->focus()}, "\cX");
 	$cui->set_binding( \&exit_dialog , "\cQ" );
 	$cui->set_binding( \&update_subscriptions, "\cR" );
+	$cui->set_binding( \&move_to_next_story, "j" );
+	$cui->set_binding( \&move_to_previous_story, "k" );
+	$cui->set_binding( \&move_to_next_subscription, "J" );
+	$cui->set_binding( \&move_to_previous_subscription, "K" );
+	$cui->set_binding( \&mark_all_as_read, "A" );
 	$cui->set_binding(sub {
 		eval "use Browser::Open qw( open_browser );";
 
@@ -150,41 +205,70 @@ sub mark_unread {
 }
 
 sub update_subscriptions {
+	status("Updating subscriptions", sub {
+		%subscriptions = $nb->get_subscriptions();
 
-	attempt_sync("Updating subscriptions");
+		my $parent = $subscription_list->parentwindow();
 
-	my %subscriptions = $nb->get_subscriptions();
-
-	$sub_list->values([ keys(%subscriptions) ]);
-	$sub_list->labels(\%subscriptions);
-	$sub_list->draw();
+		$subscription_list->values([ sort { $subscriptions{$a} <=> $subscriptions{$b} } keys(%subscriptions) ]);
+		$subscription_list->labels(\%subscriptions);
+		$subscription_list->{'-height'} = $parent->height() - 5;
+		$subscription_list->draw();
+	});
 }
 
 sub set_window {
-	$mainwindow = $cui->add( 'mainwindow', 'Window', -border => 1, -y    => 1, -bfg  => 'red' );
+	$cui->schedule_event(\&update_subscriptions);
 
-	$sub_list = $mainwindow->add(
+	$mainwindow = $cui->add(
+		'mainwindow', 'Window',
+		-border => 1,
+		-y      => 1,
+		-bfg    => 'red'
+	);
+
+	my $left_pane = $mainwindow->add(
+		'left_pane', 'Container',
+		-width		  => $subscription_list_width
+	);
+
+	$subscription_list = $left_pane->add(
 		'subscription_list', 'Listbox',
-		-width		  => $sub_list_width,
-		-values		  => [ keys(%subscriptions) ],
+		-parent		  => 'left_pane',
+		-width		  => $subscription_list_width,
+		-height		  => $mainwindow->height() - 6,
+		-values		  => [ sort { $subscriptions{$a} <=> $subscriptions{$b} } keys(%subscriptions) ],
 		-labels		  => \%subscriptions,
 		-border		  => 1,
 		-vscrollbar	  => 1,
-		-onchange	  => \&draw_posts_window
+		-onchange	  => \&draw_posts_window,
 	);
 
-	$story_list = $mainwindow->add(
+	$status_line = $left_pane->add( 
+		'status_line', 'TextViewer',
+		-parent		  => 'left_pane',
+		-text		  => "Hello, world!",
+		-y			  => $mainwindow->height() - 6,
+		-width		  => $subscription_list_width,
+		-border		  => 1,
+		-vscrollbar	  => 1,
+	);
+
+	my $right_pane = $mainwindow->add(
+		'right_pane', 'Container',
+		-x			  => $subscription_list_width + 1
+	);
+
+	$story_list = $right_pane->add(
 		'story_list', 'Listbox',
-		-x			  => $sub_list_width + 1,
 		-height		  => $story_list_height,
 		-border		  => 1,
 		-vscrollbar	  => 1,
 		-onchange	  => \&display_content
 	);
 
-	$story_container = $mainwindow->add(
+	$story_container = $right_pane->add(
 		'story_container', 'TextViewer',
-		-x			  => $sub_list_width + 1,
 		-y			  => $story_list_height,
 		-border		  => 1,
 		-wrapping	  => 1,
@@ -197,17 +281,15 @@ sub set_window {
 sub status {
 	my ($status, $cb) = @_;
 
-	$cui->status(
-		-message => $status . " ...",
-		-x => $sub_list_width,
-		-y => 0
-	);
-	&$cb;
-	$cui->nostatus();
+	$status_line->text($status);
+	if (defined($cb)) {
+		&$cb;
+		$status_line->text($status . " done");
+	}
 }
 
-sub sub_list {
-	return $sub_list;
+sub subscription_list {
+	return $subscription_list;
 }
 
 sub exit_dialog {
@@ -226,6 +308,7 @@ use utf8;
 use JSON;
 use LWP::UserAgent;
 use Text::Unidecode;
+use Time::HiRes qw/gettimeofday tv_interval/;
 use URI;
 use URI::QueryParam;
 
@@ -254,6 +337,14 @@ sub set_cui {
 	return $self;
 }
 
+sub set_status_line {
+	my ($self, $status_line) = @_;
+
+	$self->{'status_line'} = $status_line;
+
+	return $self;
+}
+
 sub get_subscriptions {
 	my ($self) = @_;
 
@@ -271,7 +362,12 @@ sub get_subscriptions {
 		next unless
 			$result->{feeds}{$feed_id}{nt} > 0 # neutral intelligence
 			|| $result->{feeds}{$feed_id}{ps} > 0; # positive intelligence
-		$subscriptions{$feed_id} = '<bold>' . $result->{feeds}{$feed_id}{feed_title} . '</bold>';
+
+		$subscriptions{$feed_id} = sprintf(
+			'<bold>%s</bold> (%d)',
+			$result->{feeds}{$feed_id}{feed_title},
+			$result->{feeds}{$feed_id}{nt} + $result->{feeds}{$feed_id}{ps}
+		);
 	}
 	
 	return %subscriptions;
@@ -306,7 +402,25 @@ sub get_stories {
 
 	my $result;
 	$self->status("Getting stories for feed", sub {
-		$result = decode_json($self->get('/reader/feed/' . $feed_id, { read_filter => 'unread' }));
+
+		my $page_stories;
+
+		for(my $page = 1; $page <= 3; $page++) {
+			$page_stories = decode_json($self->get('/reader/feed/' . $feed_id, { read_filter => 'unread', 'page' => $page }));
+
+			# if there's less than 5 items, we don't need to loop anymore
+			if (scalar(@{$page_stories->{stories}}) < 5) {
+				$page = 4;
+			}
+
+			if ($result == undef) {
+				$result = $page_stories;
+			} else {
+				foreach my $story (@{$page_stories->{stories}}) {
+					push(@{$result->{stories}}, $story);
+				}
+			}
+		}
 	});
 
 	my %stories = ();
@@ -328,10 +442,13 @@ sub get {
 	$url->path($endpoint);
 	$url->query_form_hash($parameters) if $parameters;
 
+	my $start = [gettimeofday];
+	$self->log("GET $url ...");
 	my $response = $ua->get($url);
+	$self->log(" (" . tv_interval ( $start, [gettimeofday]) . ")", 1);
 
 	if (! $response->is_success()) {
-		$self->error($response->decoded_content);
+		$self->error($response->decoded_content, $response->status_line);
 	}
 	
 	return $response->content();
@@ -342,10 +459,14 @@ sub post {
 	my $ua = $self->{ua};
 	my $url = URI->new($self->{_base_url});
 	$url->path($endpoint);
+
+	my $start = [gettimeofday];
+	$self->log("POST $url ...");
 	my $response = $ua->post($url, $parameters);
+	$self->log(" (" . tv_interval ( $start, [gettimeofday]) . ")", 1);
 
 	if (! $response->is_success()) {
-		$self->error($response->decoded_content);
+		$self->error($response->decoded_content, $response->status_line);
 	}
 	
 	return $response->content();
@@ -398,9 +519,15 @@ sub login {
 sub status {
 	my ($self, $status, $cb) = @_;
 
-	$self->{cui}->status($status . " ...");
+	if (! defined($self->{status_line})) {
+		$self->{cui}->status($status);
+	} else {
+		$self->{status_line}->text($status);
+	}
 	&$cb;
-	$self->{cui}->nostatus();
+	if (! defined($self->{status_line})) {
+		$self->{cui}->nostatus;
+	}
 }
 
 sub nostatus {
@@ -409,8 +536,19 @@ sub nostatus {
 	$self->{cui}->nostatus();
 }
 
+sub log {
+	my ($self, $message, $end_with_newline) = @_;
+
+	open(my $fh, ">>", "messages.log") || die "Couldn't write to messages.log file: $!";
+	print $fh $message;
+	if ($end_with_newline == 1) {
+		print $fh "\n";
+	}
+	close($fh);
+}
+
 sub error {
-	my ($self, $content) = @_;
+	my ($self, $content, $status_line) = @_;
 
 	$self->{cui}->error(
 		-message => $content,
@@ -419,6 +557,7 @@ sub error {
 
 	open(my $fh, ">>", "error.log") || die "Couldn't write to error.log file: $!";
 	print $fh $content;
+	print $fh $status_line;
 	close($fh);
 
 	$self->{cui}->exit_curses();
